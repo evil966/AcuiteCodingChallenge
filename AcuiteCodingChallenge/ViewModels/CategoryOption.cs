f5c904ea0f2b8691a62de45239bbba6afccf4363
Edwin Villapando <edwinvillapando@gmail.com>
2023-04-29 15:17:15 +0800

    use dictionary instead of splitting from string as key/value pair

--- AcuiteCodingChallenge/ViewModels/CategoryOption.cs
+++ AcuiteCodingChallenge/ViewModels/CategoryOption.cs
@@ -1,19 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using AcuiteCodingChallenge.Core.Constants;
+using System.Collections.Generic;
 
 namespace AcuiteCodingChallenge.ViewModels
 {
     public static class CategoryOptions
     {
-        public static List<string> Get()
+        public static Dictionary<string, string> Get()
         {
-            return new List<string>()
-            { 
-                "Computer Science|ComputerScience",
-                "Physics|Physics",
-                "History|History",
-                "Geography|Geography",
-                "Travel|Travel",
-                "Fiction|Fiction"
+            return new Dictionary<string, string>
+            {
+                { "Computer Science", BookCategory.ComputerScience.ToString() },
+                { "Physics", BookCategory.Physics.ToString() },
+                { "History", BookCategory.History.ToString() },
+                { "Geography", BookCategory.Geography.ToString() },
+                { "Travel", BookCategory.Travel.ToString() },
+                { "Fiction", BookCategory.Fiction.ToString() }
             };
         }
     }
